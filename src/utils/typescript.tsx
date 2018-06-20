fff7885de1a790edeb83560a543a1630ee2206e5
Tobias de Groen <tdgroen@zaibot.net>
2018-06-20 09:36:49 +0200

    fix(project): typescript 2.9, Omit Diff

--- src/utils/typescript.tsx
+++ src/utils/typescript.tsx
@@ -1,5 +1,2 @@
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
-export type Omit<T, K extends keyof T> = Pick<T, Diff<Extract<keyof T, string>, Extract<K, string>>>;
-
-// TODO: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/26510
-// https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#typescript-29
+export type Diff<T extends PropertyKey, U extends PropertyKey> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+export type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
