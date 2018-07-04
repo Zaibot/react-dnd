aa543ecb18a344bb497ddd6025d8d8bd8b62f782
Tobias de Groen <tdgroen@zaibot.net>
2018-07-04 12:14:31 +0200

    chore(storybook): disable non working

--- stories/dnd.tsx
+++ stories/dnd.tsx
@@ -5,7 +5,6 @@ import { Droppable } from "../src/droppable";
 import { DragLayer } from "../src/drag-layer";
 import { Measure } from "../src/measure";
 import { DragHandle } from "../src/drag-handle";
-import { closestCorner } from "../src/strategy-closest-corner";
 import { DraggableContext, DraggingProvider } from "../src/core";
 
 const stories = storiesOf("react-dnd/Drag&Drop", module);
@@ -20,11 +19,6 @@ const Yellow = ({ ...props }: React.HTMLProps<HTMLDivElement>) => <div
   {...props}
   style={{ ...props.style, background: `#ffa`, display: `inline-block`, padding: `2rem` }} />;
 
-const orderingData = [
-  { id: 1, text: `Hello` },
-  { id: 2, text: `World` },
-  { id: 3, text: `!` },
-];
 
 stories
   .add(`Basic`, () => (
@@ -65,48 +59,48 @@ stories
         {/* : <Yellow {...dragContainerProps} innerRef={ref}>{dragging}<DragHandle>Handle</DragHandle> Drag me</Yellow> */}
       </DraggableContext>
     </DraggingProvider>
-  ))
-  .add(`Ordering`, () => (
-    <DraggingProvider>
-      {/* <PositionContainer>
-        {({ refContainer, registries }) => (
-          <Droppable onDrop={action(`onDrop`)} onDragOver={action(`onDragOver`)} onDragOut={action(`onDragOut`)} refTracking={refContainer}>
-            {({ isDropping, dropProps, trackingProps, droppingPosition }) => (
-              <Red {...trackingProps} {...dropProps} innerRef={trackingProps.ref}>
-                {orderingData.map((item) => (
-                  <PositionPublisher keyData={item}>
-                    {({ refContainer }) => (
-                      <Draggable dataKey={item} onDragEnd={action(`onDragEnd`)} onDragMove={undefined} onDragStart={action(`onDragStart`)} refTracking={refContainer}>
-                        {({ isDragged, dragContainerProps, dragHandleProps, trackingProps, dragging }) => (
-                          isDragged
-                            ? <Blue {...dragContainerProps} {...dragHandleProps} innerRef={trackingProps.ref}>{dragging}{item.text}</Blue>
-                            : <Yellow {...dragContainerProps} {...dragHandleProps} innerRef={trackingProps.ref}>{dragging}{item.text}</Yellow>
-                        )}
-                      </Draggable>
-                    )}
-                  </PositionPublisher>
-                ))}
-
-                <ol>
-                  {registries.map(({ getBounds, key }) => (
-                    <li>{key.text} ({getBounds().left}:{getBounds().top}, {getBounds().width}w{getBounds().height}h)</li>
-                  ))}
-                </ol>
-                <p style={{ whiteSpace: 'pre' }}>
-                  {isDropping
-                    ? <span>Dropping: {(() => {
-                      const elements = registries.map(({ getBounds, key }) => ({ bounds: getBounds(), key, order: orderingData.indexOf(key) }));
-                      const closest = closestCorner.horizontal(droppingPosition, elements);
-                      return (
-                        <span>{closest.isBefore ? 'Before' : 'After'} {closest.element.key.text}</span>
-                      );
-                    })()}</span>
-                    : `Drag something`}
-                </p>
-              </Red>
-            )}
-          </Droppable>
-        )}
-      </PositionContainer> */}
-    </DraggingProvider>
   ));
+  // .add(`Ordering`, () => (
+  //   <DraggingProvider>
+  //      <PositionContainer>
+  //       {({ refContainer, registries }) => (
+  //         <Droppable onDrop={action(`onDrop`)} onDragOver={action(`onDragOver`)} onDragOut={action(`onDragOut`)} refTracking={refContainer}>
+  //           {({ isDropping, dropProps, trackingProps, droppingPosition }) => (
+  //             <Red {...trackingProps} {...dropProps} innerRef={trackingProps.ref}>
+  //               {orderingData.map((item) => (
+  //                 <PositionPublisher keyData={item}>
+  //                   {({ refContainer }) => (
+  //                     <Draggable dataKey={item} onDragEnd={action(`onDragEnd`)} onDragMove={undefined} onDragStart={action(`onDragStart`)} refTracking={refContainer}>
+  //                       {({ isDragged, dragContainerProps, dragHandleProps, trackingProps, dragging }) => (
+  //                         isDragged
+  //                           ? <Blue {...dragContainerProps} {...dragHandleProps} innerRef={trackingProps.ref}>{dragging}{item.text}</Blue>
+  //                           : <Yellow {...dragContainerProps} {...dragHandleProps} innerRef={trackingProps.ref}>{dragging}{item.text}</Yellow>
+  //                       )}
+  //                     </Draggable>
+  //                   )}
+  //                 </PositionPublisher>
+  //               ))}
+
+  //               <ol>
+  //                 {registries.map(({ getBounds, key }) => (
+  //                   <li>{key.text} ({getBounds().left}:{getBounds().top}, {getBounds().width}w{getBounds().height}h)</li>
+  //                 ))}
+  //               </ol>
+  //               <p style={{ whiteSpace: 'pre' }}>
+  //                 {isDropping
+  //                   ? <span>Dropping: {(() => {
+  //                     const elements = registries.map(({ getBounds, key }) => ({ bounds: getBounds(), key, order: orderingData.indexOf(key) }));
+  //                     const closest = closestCorner.horizontal(droppingPosition, elements);
+  //                     return (
+  //                       <span>{closest.isBefore ? 'Before' : 'After'} {closest.element.key.text}</span>
+  //                     );
+  //                   })()}</span>
+  //                   : `Drag something`}
+  //               </p>
+  //             </Red>
+  //           )}
+  //         </Droppable>
+  //       )}
+  //     </PositionContainer>
+  //   </DraggingProvider>
+  // ));
